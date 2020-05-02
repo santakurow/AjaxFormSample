592f169375e71163e174182f8893c374b05ba88c
santakurow <newtakuproj0730@gmail.com>
2020-05-02 22:34:37 +0900

    form_with for ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,25 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.order(created_at: :desc)
+    @task = Task.new
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    respond_to do |format|
+      if @task.save
+        format.js
+      end
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:text)
   end
 end
